c2297decf3016a973a520d23762bf4ef6c85cd71
Nellia Salimova <salimova.nellia@gmail.com>
2020-06-11 18:40:04 +0300

    Delete button

--- src/app/node/node.component.ts
+++ src/app/node/node.component.ts
@@ -1,5 +1,6 @@
 import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 import {Node} from "../../../model/node";
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-node',
@@ -7,13 +8,14 @@ import {Node} from "../../../model/node";
   styleUrls: ['./node.component.scss']
 })
 export class NodeComponent implements OnInit {
+  @ViewChild('newItem', {static: false}) newItem: ElementRef;
+
   @Input()
   private node: Node;
 
   listOpened: boolean = false;
 
   constructor() { }
-
   ngOnInit() {
   }
 
@@ -21,4 +23,14 @@ export class NodeComponent implements OnInit {
     this.listOpened = !this.listOpened;
     event.target.parentElement.classList.toggle('open');
   }
+
+  addNewItem() {
+    let valueInput = this.newItem.nativeElement.value;
+    if (valueInput) {
+      let newItem = new Node(valueInput, '', []);
+      this.node.children.push(newItem);
+    }
+  }
+
+
 }
